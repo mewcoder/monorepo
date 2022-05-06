9a4f3a31bee70c4d01b62b6ec1ab571d560b961c
zhaodacheng5 <zhaodacheng5@hikvision.com.cn>
2022-05-06 14:07:21 +0800

    feat: mod lerna

---
+++ npm-demo/test/index.js
@@ -0,0 +1,20 @@
+// npm notice created a lockfile as package-lock.json. You should commit this file.
+// npm WARN npm-test-dev@1.2.0 requires a peer of co@^4.6.0 but none is installed. You must install peer dependencies yourself.
+// npm WARN test@1.0.0 No description
+// npm WARN test@1.0.0 No repository field.
+
+
+// peerDependencies: 不会报错，会提醒
+
+
+
+
+// yarn install v1.22.17
+// info No lockfile found.
+// [1/4] Resolving packages...
+// [2/4] Fetching packages...
+// [3/4] Linking dependencies...
+// warning " > npm-test-dev@1.2.0" has unmet peer dependency "co@^4.6.0".
+// [4/4] Building fresh packages...
+
+// success Saved lockfile.
